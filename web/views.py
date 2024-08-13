195040bfd3cf461a5a6c6c6e70ebfc37ba62fe43
XR-Ruben <INVITADOJR@MacBook-Pro-de-Ruben.local>
2024-08-12 22:58:04 -0400

    Se modificó el stylo y formulario, dia 12 HITT3

--- web/views.py
+++ web/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Flan, ContactForm
-from .forms import ContactFormForm
+from .forms import ContactFormForm, ContactModelForm
 # ----------------------------------------------------------------------------------------------
 ## VIEWS DE PROYECTO ONLYFLANS
 
@@ -39,17 +39,11 @@ def footer(req):
 
 
 def welcome(request):
-    private_flans = [{"name": "Pineapple Dessert 'Cream' - Dutch", "image_url": "https://www.cookiedoughandovenmitt.com/wp-content/uploads/2021/03/Mint-Chocolate-Dessert-10-Photo-Cookie-Dough-and-Oven-Mitt.jpg", "description": "Reserve some of the crushed pineapple for garnish. Sprinkle gelatin over water in small saucepan; "},
-                     {"name": "Guiltless Pumpkin Dessert", "image_url": "https://supersisterfitness.com/wp-content/uploads/2013/11/pumpkinpie-1024x10241.jpg", "description": "Mix the pudding and the milk together as instructed on the box. Allow to set for 5 minutes. Mix together the pumpkin and pumpkin pie spice. Then fold in (don't beat) with the pudding mixture. Layer on the crumbs and then top with Cool Whip. "},]
-    return render(request,'welcome.html',{'private_flans': private_flans})
     private_flans = Flan.objects.filter(is_private=True)
     return render(request, 'welcome.html', {"private_flans": private_flans})
 
 
-
-
 # *  <!-- apply FORM contacto -->
-
 
 def contacto(request):
     if request.method == 'POST':
@@ -61,29 +55,16 @@ def contacto(request):
             ContactForm.objects.create(**form.cleaned_data) # pasamos la data del diccionario .cleaned_data a argumentos
             return HttpResponseRedirect('/exito')
     else: 
-        form = ContactFormForm()    
+        form = ContactModelForm()    
         return render(request, 'contacto.html', {'form':form})
 
 # *  --- apply ContactModelForm ---
-# from .forms import ContactModelForm  # Asegúrate de importar el formulario correcto
 
 # # # *  <!-- apply MODEL-FORM contacto -->
-# def contacto(request):
-#     return render(request, 'contacto.html', {})
+
 # *  --- apply ContactModelForm ---
 
 
-# * <!-- apply DAY 11 - 12 -->
 def exito(request):
     return render(request, 'exito.html', {})
 
-
-
-
-
-
-# def welcome(request):
-#     # private_flans = [{"name": "Pineapple Dessert 'Cream' - Dutch", "image_url": "https://www.cookiedoughandovenmitt.com/wp-content/uploads/2021/03/Mint-Chocolate-Dessert-10-Photo-Cookie-Dough-and-Oven-Mitt.jpg", "description": "Reserve some of the crushed pineapple for garnish. Sprinkle gelatin over water in small saucepan; "},
-#     #                  {"name": "Guiltless Pumpkin Dessert", "image_url": "https://supersisterfitness.com/wp-content/uploads/2013/11/pumpkinpie-1024x10241.jpg", "description": "Mix the pudding and the milk together as instructed on the box. Allow to set for 5 minutes. Mix together the pumpkin and pumpkin pie spice. Then fold in (don't beat) with the pudding mixture. Layer on the crumbs and then top with Cool Whip. "},]
-#     private_flans = Flan.objects.filter(is_private=True)
-#     return render(request,'welcome.html',{'ruben': private_flans})
